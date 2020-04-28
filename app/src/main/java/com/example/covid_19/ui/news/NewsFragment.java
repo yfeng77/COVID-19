b285d6898c90d91870885feebeb918d82c483693
Yingqi Feng <64438141+yfeng77@users.noreply.github.com>
2020-04-28 00:54:05 -0400

    Completed the News section and changed some appearances

--- app/src/main/java/com/example/covid_19/ui/news/NewsFragment.java
+++ app/src/main/java/com/example/covid_19/ui/news/NewsFragment.java
@@ -1,26 +1,136 @@
 package com.example.covid_19.ui.news;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.covid_19.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class NewsFragment extends Fragment {
 
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+    public static final String TAG = "MyTag";
+    RequestQueue requestQueue;  // Assume this exists.
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_news, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // get the listview
+        expListView = (ExpandableListView) view.findViewById(R.id.newlist);
+
+        // preparing list data
+       prepareListData();
+
+
+    }
+
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        // Instantiate the cache
+        Cache cache = new DiskBasedCache(getActivity().getCacheDir(), 1024 * 1024); // 1MB cap
+
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        Network network = new BasicNetwork(new HurlStack());
+
+        // Instantiate the RequestQueue with the cache and network.
+        requestQueue = new RequestQueue(cache, network);
+        // Start the queue
+        requestQueue.start();
+
+        String url = "https://newsapi.org/v2/everything?q=COVID&from=2020-04-27&sortBy=publishedAt&apiKey=e67d205313494e0b963b270d14823340&pageSize=10&page=1";
+        // Formulate the request and handle the response.
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+                    public void onResponse(JSONObject response) {
+                        try {
+                            listDataHeader = new ArrayList<String>();
+                            listDataChild = new HashMap<String, List<String>>();
+
+                            JSONArray array = response.getJSONArray("articles");
+
+                            for (int i = 0; i < array.length(); i++) {
+                                JSONObject obj = array.getJSONObject(i);
+                                // Adding child data
+                                String title = obj.getString ("title");
+                                listDataHeader.add(title);
+
+                                // Adding child data
+                                List<String> description = new ArrayList<String>();
+                                String descriptionlist = obj.getString ("description");
+                                String url = obj.getString ("url");
+                                description.add(descriptionlist);
+                                description.add(url);
+
+                                listDataChild.put(listDataHeader.get(i), description); // Header, Child data
+                            }
+
+                            listAdapter = new ExpandableListAdapter(getActivity(), listDataHeader, listDataChild);
+                            // setting list adapter
+                            expListView.setAdapter(listAdapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getActivity(), error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        // Set the tag on the request.
+        jsonObjectRequest.setTag(TAG);
+
+        // Add the request to the RequestQueue.
+        requestQueue.add(jsonObjectRequest);
+
     }
 }
